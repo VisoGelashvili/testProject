cc7a66c03789259734f4f5d245934ce1cbea803d
VisoGelashvili <156338588+VisoGelashvili@users.noreply.github.com>
2024-06-14 15:25:02 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let question = prompt('Enter your age!');
+let question = Number(prompt('Enter your age! To quit app: type "quit" or "q"'));
 
 while(question !== 'quit' && question !== 'q'){
     if(question > 0 && question < 17){
@@ -8,30 +8,38 @@ while(question !== 'quit' && question !== 'q'){
             console.log('You are male');
         } else if (gender === 2){
             console.log('You are female');
+        } else {
+            console.log('Incorrect number!');
         };
     } else if (question > 17 && question < 60){
         gender = Number(prompt('Enter your gender: 1.Male or 2.Female'));
         if(gender === 1){
+            console.log('You are an adult!');
             console.log('You are male');
         } else if (gender === 2){
             console.log('You are female');
+        } else {
+            console.log('Incorrect number!');
         };
-        console.log('You are an adult!');
-    } else if(question > 59 && question < 65 ){
+    } else if(question > 59 && question <= 65 ){
         gender = Number(prompt('Enter your gender: 1.Male or 2.Female'));
         if(gender === 2){
             console.log('You are female pensioner!');
-        }else if(gender === 1){
+        } else if(gender === 1){
             console.log('You are pensioner!');
-        }
+        } else {
+            console.log('Incorrect number!');
+        };
     } else if (question >65 && question < 120){
         gender = Number(prompt('Enter your gender: 1.Male or 2.Female'));
         if(gender === 1){
             console.log('You are male pensioner!');
-        }else if (gender === 2){
+        } else if (gender === 2){
             console.log('You are female pensioner!');
-        }
+        } else {
+            console.log('Incorrect number!');
+        };
     }
-    question = prompt('Enter your age! To exit app: type "quit" or "q"');
+    question = prompt('Enter your age!');
 }
 console.log('You quit the app!');
